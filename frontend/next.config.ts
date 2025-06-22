94142813d9e535394fc18dc0434a4d7e27dea82f
abdobargush <zezo.mohamed00@gmail.com>
2025-06-22 10:51:34 +0400

    Specify webpack alias and include next-env for deply

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -9,6 +10,10 @@ const nextConfig: NextConfig = {
         hostname: "*",
       },
     ],
+  },
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    return config;
   },
 };
 
